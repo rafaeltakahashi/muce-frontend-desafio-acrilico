1cacdfbfec1ffce52f0ce44cf092474ee07a0c43
Rafael Takahashi <rtakahashi.muralis@alelo.com.br>
2024-05-08 09:33:27 -0300

    Bugfix: Conversão de Color para string hex.

--- lib/molecules/glass_sample.dart
+++ lib/molecules/glass_sample.dart
@@ -18,12 +18,11 @@ class GlassSample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Get the color's integer value, which represents alpha, red, green
-    // and blue in the format 0xffrrggbb. Then, modulus with ff000000
-    // and format as hexadecimal.
+    // and blue in the format 0xffrrggbb. Then, mask to remove alpha.
     // Add the blur size and the opacity to get a label that says something
     // like "#808080 50%, 10 blur"
     var labelText =
-        "#${(tint.value % 16711680).toRadixString(16)}\n${opacity * 100}%\n$blurSize blur";
+        "#${(tint.value & 0xFFFFFF).toRadixString(16).padLeft(6, '0').toUpperCase()}\n${opacity * 100}%\n$blurSize blur";
 
     return Column(
       mainAxisSize: MainAxisSize.min,
